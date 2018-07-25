53c79b5a3113ee3657cfff31993784dc719f8e3b
juaninho10 <juanmeanoc@gmail.com>
2018-07-25 12:08:02 -0400

    Update

---
+++ 26-single-file-components/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <persona></persona>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import persona from './components/persona.vue'
+
+export default {
+  //name: 'app',
+  components: {
+    persona
+  }
+}
+</script>
+
+<style>
+
+</style>
